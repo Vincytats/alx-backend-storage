9717a8f1e1ef6d171de6ff795213db96120b90d0
Vincytats <vincenttats@gmail.com>
2024-07-16 13:09:08 -0700

    Commit

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
-""" 10-main """
-from pymongo import MongoClient
+'''Task 10's module.
+'''
 
-list_all = __import__('8-all').list_all
-update_topics = __import__('10-update_topics').update_topics
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
-    update_topics(school_collection, "Holberton school", ["Sys admin", "AI", "Algorithm"])
-
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('topics', "")))
-
-    update_topics(school_collection, "Holberton school", ["iOS"])
-
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('topics', "")))
+def update_topics(mongo_collection, name, topics):
+    '''Changes all topics of a collection's document based on the name.
+    '''
+    mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
+    )
